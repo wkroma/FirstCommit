35bbbb35c95d41f877742512a06bba1adaacf275
wkroma <wkroma@163.com>
2025-04-16 21:50:27 +0800

    commit info

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("11111111111111111");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("222222222222");
+    }
+}
---
+++ READE.md
@@ -0,0 +1,1 @@
+# FreeTimeProject
